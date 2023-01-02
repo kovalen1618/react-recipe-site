ad7de383795ef9ef12be36d289f23998fdd055f2
kovalen1618 <kovalen1.618@gmail.com>
2023-01-02 11:41:22 -0600

    Fleshed out Recipe page

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import { useFetch } from '../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
+
 import './Recipe.css'
 
 function Recipe() {
+    const { id } = useParams();
+    const url = 'http://localhost:3000/recipes/' + id;
+
+    const { isPending, error,  data: recipe } = useFetch(url);
+
     return (
-        <div>
-            Recipe
+        <div className='recipe'>
+            {error && <p className='error'>{error}</p>}
+            {isPending && <p className='loading'>Loading...</p>}
+            {recipe && <h1>{recipe.title}</h1>}
         </div>
     )
 }
