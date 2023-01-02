5078a856b262d0a6f4a6e0ca1af4de23e8aed6c8
kovalen1618 <kovalen1.618@gmail.com>
2023-01-02 10:42:11 -0600

    Create list of Recipes component and styles

---
+++ src/components/RecipeList.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+
+import './RecipeList.css'
+
+// RecipeList() is accepting a property (prop) called recipes which it receives from its parent component: Home
+function RecipeList({ recipes }) {
+    return (
+        <div className="recipe-list">
+            {recipes.map(recipe => (
+                <div key={recipe.id} className="card">
+                    <h3>{recipe.title}</h3>
+                    <p>{recipe.cookingTime} to make.</p>
+                    <div>{recipe.method.substring(0, 100)}...</div>
+                    <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default RecipeList
